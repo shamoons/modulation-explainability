5140262c2377948cbac04d0aca4d3599ca816615
Shamoon Siddiqui <shamoon@develop.io>
2024-10-21 11:24:34 -0400

    Refactor learning rate scheduler and optimizer in train_constellation.py

--- src/train_constellation.py
+++ src/train_constellation.py
@@ -86,13 +86,13 @@ def main(checkpoint=None, batch_size=64, snr_list=None, mods_to_process=None, ep
     criterion_snr = nn.CrossEntropyLoss()  # Custom SNR loss
 
     # Initialize optimizer
-    optimizer = optim.Adam(model.parameters(), lr=0.00001)
+    optimizer = optim.Adam(model.parameters(), lr=0.000001)
 
     # Calculate the number of batches per epoch
     batches_per_epoch = len(train_loader)
 
     # Define number of cycles
-    num_cycles = 4
+    num_cycles = 8
 
     # Total number of batches over all epochs
     total_batches = batches_per_epoch * epochs
@@ -101,7 +101,7 @@ def main(checkpoint=None, batch_size=64, snr_list=None, mods_to_process=None, ep
     step_size_up_down = total_batches // (num_cycles * 2)
 
     # Add learning rate scheduler with dynamic step_size_up
-    scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.00001, max_lr=0.0001, step_size_up=step_size_up_down, step_size_down=step_size_up_down, mode='triangular2', cycle_momentum=False)
+    scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.000001, max_lr=0.0001, step_size_up=step_size_up_down, step_size_down=step_size_up_down, mode='triangular2', cycle_momentum=False)
 
     # Determine device (CUDA, MPS, or CPU)
     device = get_device()
